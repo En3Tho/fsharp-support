1c1b380734277679496ee00f4707a54b6a74b32b
En3Tho <37334640+En3Tho@users.noreply.github.com>
2021-01-17 14:58:27 +0300

    Added copy scripts

---
+++ CopyFCSDebug.ps1
@@ -0,0 +1,5 @@
+Copy-Item "..\visualfsharp\artifacts\bin\fcs\Debug\netstandard2.0\FSharp.Compiler.Service.dll" -Destination "ReSharper.FSharp\src\FSharp.Common\bin\Debug\net461\"
+Copy-Item "..\visualfsharp\artifacts\bin\fcs\Debug\netstandard2.0\FSharp.Compiler.Service.dll" -Destination "ReSharper.FSharp\src\FSharp.ProjectModelBase\bin\Debug\net461\"
+Copy-Item "..\visualfsharp\artifacts\bin\fcs\Debug\netstandard2.0\FSharp.Compiler.Service.dll" -Destination "ReSharper.FSharp\src\FSharp.Psi\bin\Debug\net461\"
+Copy-Item "..\visualfsharp\artifacts\bin\fcs\Debug\netstandard2.0\FSharp.Compiler.Service.dll" -Destination "ReSharper.FSharp\src\FSharp.Psi.Features\bin\Debug\net461\"
+Write-Host "0"
